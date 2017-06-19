31ac6446108179c53f305da47607829c693ff346
James Sutton <james.sutton1211@gmail.com>
2017-06-19 08:02:44 -0700

    pulling cfg out of detectors.py

--- src/detectors.py
+++ src/detectors.py
@@ -1,9 +1,24 @@
-from cfg import Config as cfg
 from other import prepare_img, normalize
 import numpy as np
 from utils.cpu_nms import cpu_nms as nms
 from text_proposal_connector import TextProposalConnector
 
+class Config:
+    MEAN=np.float32([102.9801, 115.9465, 122.7717])
+    TEST_GPU_ID=0
+    SCALE=600
+    MAX_SCALE=1000
+
+    LINE_MIN_SCORE=0.7
+    TEXT_PROPOSALS_MIN_SCORE=0.7
+    TEXT_PROPOSALS_NMS_THRESH=0.3
+    MAX_HORIZONTAL_GAP=50
+    TEXT_LINE_NMS_THRESH=0.3
+    MIN_NUM_PROPOSALS=2
+    MIN_RATIO=1.2
+    MIN_V_OVERLAPS=0.7
+    MIN_SIZE_SIM=0.7
+    TEXT_PROPOSALS_WIDTH=16
 
 class TextProposalDetector:
     """
